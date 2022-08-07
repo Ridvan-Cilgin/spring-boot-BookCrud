c60131eb41430086972dd7b3d5ad1fbc2fdacfd1
Rıdvan Çılgın <ridvanclgn1991@gmail.com>
2022-08-07 15:32:31 +0300

    Initial commmit

---
+++ src/test/java/com/ridvancilgin/springbootbookcrud/SpringBootBookCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridvancilgin.springbootbookcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBookCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
